1e4e0c2833d308e75dfc0bc4987fcf64680d28fc
Alex Mullen <mullena63@gmail.com>
2017-07-11 14:04:59 -0400

    initial project commit - basic layout, positionreport and opendoor created

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		float OpenAngle = 70.0f;
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate;
+	UPROPERTY(EditAnywhere)
+		AActor* OpeningActor;
+		
+	
+};
